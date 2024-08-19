4d687bdc373c705582c9b16471723a41f05b4590
moulaali-duplo <moulaali@duplocloud.net>
2024-08-19 16:44:52 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! This is a simple Node.js app running in a Docker container.');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
